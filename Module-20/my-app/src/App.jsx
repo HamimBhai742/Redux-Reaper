7704ae449d2b3543f3dc4e556420c5ad21c37763
HamimBhai742 <omagootruelove147@gmail.com>
2025-06-26 14:12:25 +0600

    complete 20.9

--- Module-20/my-app/src/App.jsx
+++ Module-20/my-app/src/App.jsx
@@ -1,31 +1,64 @@
-import { useState } from "react"
+import { useReducer } from 'react';
+// import { useState } from 'react';
 
 function App() {
-const [submit,setSubmit]=useState({
-  name:"",
-  email:"",
-  phone:""
-})
+  // const [submit,setSubmit]=useState({
+  //   name:"",
+  //   email:"",
+  //   phone:""
+  // })
 
-const userInfo=(input,value)=>{
-  setSubmit({
-    ...submit,[input]:value
-  })
-}
-const handelSubmit=(e)=>{
-  e.preventDefault()
-console.log(submit)
-}
+  const intialState = {
+    name: '',
+    email: '',
+    phone: '',
+  };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'FIELD_UPDATE':
+        return {
+          ...state,
+          [action.payload.field]: action.payload.value,
+        };
+
+      default:
+        state;
+    }
+  };
+
+  const [userInfo, dispatch] = useReducer(reducer, intialState);
+
+  const handelOnChange = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: 'FIELD_UPDATE',
+      payload: {
+        field: e.target.name,
+        value: e.target.value,
+      },
+    });
+  };
+  console.log(userInfo)
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    console.log(userInfo);
+
+  };
+
+  // const handelSubmit=(e)=>{
+  //   e.preventDefault()
+
+  // }
   return (
     <div>
-      <form className="form" onSubmit={handelSubmit}>
-        <input type="text" placeholder='name' onChange={(e)=>userInfo("name",e.target.value)}/>
-        <input type="email" placeholder='email' onChange={(e)=>userInfo("email",e.target.value)} />
-        <input type="text" placeholder='phone' onChange={(e)=>userInfo("phone",e.target.value)} />
+      <form className='form' onSubmit={handelSubmit}>
+        <input type='text' name='name' placeholder='name' onChange={handelOnChange} />
+        <input type='email' name='email' placeholder='email' onChange={handelOnChange} />
+        <input type='text' name='phone' placeholder='phone' onChange={handelOnChange} />
         <button type='submit'>Submit</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
